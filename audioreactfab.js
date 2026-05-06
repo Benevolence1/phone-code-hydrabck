cf49f1c469bc958e03b18fc87ef7c25e85ff9189
Azaraus <161731095+Benevolence1@users.noreply.github.com>
2026-05-06 02:14:31 -0400

    Update color settings and add render function

--- audioreactfab.js
+++ audioreactfab.js
@@ -5,18 +5,19 @@ const hydra = new Hydra({
 
 
 
+
 a.setBins(4)
 a.fft[0]
 
 testRun = 15
 osc(testRun, 0.01, 0.01).blend(noise(8, 0.02, 0.01), 9)
-.color(-1, -1, -1)
+.color(15, 1, 1)
 .modulateRotate(noise(7, 0.0060, 0), () => a.fft[0]* 3.4)
 .pixelate(4, 4)
 .modulateRepeat(osc(7, 0.09, 0),  0.4, -2, 0.4, -2)
 .modulateScale(noise(6, 0.01, 1),0.4)
 // .kaleid()
-.color(-2, -2, -2)
+.color(2, 2, 2)
 // .saturate(osc(2, 5, 12), -2)
 .pixelate(90, 90)
 .scale(1)
